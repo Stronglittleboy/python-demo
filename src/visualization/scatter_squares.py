b288fa79c7ea05332368aefbc787f9816aa2ddb4
hlw <13598055090@163.com>
2021-03-31 15:37:22 +0800

    feat:  1.撒点图

--- src/visualization/scatter_squares.py
+++ src/visualization/scatter_squares.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
-x_value = [1, 2, 3, 4, 5]
-y_value = [1, 4, 9, 16, 25]
+x_value = list(range(1, 1001))
+y_value = [x ** 2 for x in x_value]
 
-plt.scatter(x_value, y_value, s=200)
+plt.scatter(x_value, y_value, c=(0, 0, 0.5), edgecolors='none', s=40)
 plt.title("我的散点图", fontsize=24)
 plt.xlabel("VALUE", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 plt.tick_params(axis="both", which="major", labelsize=14)
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
